b2b94e7897edb4217ee25fd995efc7bb4ca1077d
DmitryZhokhov <dmitryzhokhov@gmail.com>
2022-07-31 18:39:27 +0300

    update

--- tests/raiffeisen_tests.spec.ts
+++ tests/raiffeisen_tests.spec.ts
@@ -1,15 +1,15 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test.describe.parallel('Raiffeisen Tests', () => {
 
   test('Order debit card by Male', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/')
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Playwright/)
 
   // create a locator
-  const getStarted = page.locator('text=Get Started');
+  const getStarted = page.locator('text=Get Started')
 
   // Expect an attribute "to be strictly equal" to the value.
   await expect(getStarted).toHaveAttribute('href', '/docs/intro');
@@ -19,9 +19,9 @@ test.describe.parallel('Raiffeisen Tests', () => {
 
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
-});
+  })
 
   test('Order debit card by Male', async ({ page }) => {
 
-})
+  })
 })
