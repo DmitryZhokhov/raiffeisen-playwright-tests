c52bd9032f71e4803c23ed64e06f254df399b48c
DmitryZhokhov <dmitryzhokhov@gmail.com>
2022-08-05 18:53:49 +0300

    update

--- src/helpers/interceptionResult.spec.ts
+++ src/helpers/interceptionResult.spec.ts
@@ -4,7 +4,7 @@ export async function interceptionResponseResult(page: Page, phone: string, toke
   const requestURL = 'https://oapi.raiffeisen.ru/api/forms/public/v1.0/forms/debit-card-single-field/66/answers'
   page.on('request', (request) => {
     if (request.url() === requestURL) {
-      console.log(request.method(), request.url(), request.postDataJSON())
+      //console.log(request.method(), request.url(), request.postDataJSON())
       expect(request.postDataJSON()).toMatchObject({ auth: { phone: `${phone}`, token: `${token}` } })
       expect(request.postDataJSON()).toMatchObject({
         answer: {
